ae19a8cdb83f47c9e2691cd709a4b77fe4be2e98
silviodeigiudici <silviodeigiudici@gmail.com>
2020-12-17 23:17:40 +0100

    feat(Compute-response-time): we add a script to compute global response time

---
+++ cost_constraint/total_response_time.py
@@ -0,0 +1,118 @@
+import json
+
+import resource, sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+sys.setrecursionlimit(10**6)
+
+
+class Container:
+    def __init__(self):
+        self.data = None
+        self.sons = []
+
+def setup_dict(json_data):
+    dict_global = {}
+
+    for node in json_data:
+        id_node = node["id"]
+
+        if id_node not in dict_global:
+            dict_global[id_node] = Container()
+        
+        dict_global[id_node].data = node
+
+        next_id = dict_global[id_node].data["upper_node"]
+
+
+        if next_id not in dict_global:
+            dict_global[next_id] = Container()
+
+        dict_global[next_id].sons.append(dict_global[id_node])
+
+    return dict_global
+
+
+#######################
+#UTILS
+###################
+
+def get_max(l1, l2):
+    m1 = max(l1[0], l2[0])
+    m2 = max(l1[1], l2[1])
+    m3 = max(l1[2], l2[2])
+    m4 = max(l1[3], l2[3])
+    return (m1, m2, m3, m4)
+
+def sum_tuple(l1, l2):
+    return (l1[0] + l2[0], l1[1] + l2[1], l1[2] + l2[2], l1[3] + l2[3])
+
+
+####################
+#response time 
+##################
+
+RESPONSE_TYPE = 2 #0->all, 1->regional + local, 2->local
+
+
+def compute_response(container):
+    
+    #################################
+    #taking my response times
+    ######################à
+    if container.data["type"] == "wan":
+        my_response = (0.0, 0.0, 0.0, 0.0)
+    elif container.data["type"] == "lan":
+        telemetry_response = container.data["lan_out"]["telemetry"]["response_time"]
+        transition_response = container.data["lan_out"]["transition"]["response_time"]
+        command_response = container.data["lan_in"]["command"]["response_time"]
+        batch_response = container.data["lan_out"]["batch"]["response_time"]
+
+        my_response = (telemetry_response, transition_response, command_response, batch_response)
+    else:
+        telemetry_response = container.data["parameters"]["telemetry"]["response_time"]
+        transition_response = container.data["parameters"]["transition"]["response_time"]
+        command_response = container.data["parameters"]["command"]["response_time"]
+        batch_response = container.data["parameters"]["batch"]["response_time"]
+
+        my_response = (telemetry_response, transition_response, command_response, batch_response)
+
+    ##################################
+    #calling all my sons
+    #############################
+    max_response = (0.0, 0.0, 0.0, 0.0)
+    for son in container.sons:
+        ret_response = compute_response(son)
+        max_response = get_max(max_response, ret_response)
+    
+
+    ############################
+    #returning values based on who I am
+    ############################
+    if RESPONSE_TYPE == 0:
+        return sum_tuple(max_response, my_response)
+    if RESPONSE_TYPE == 1:
+        if container.data["type"] == "node" and container.data["node_type"] == "central":
+            return max_response
+        else:
+            return sum_tuple(max_response, my_response)
+    if RESPONSE_TYPE == 2:
+        if container.data["type"] == "node" and (container.data["node_type"] == "central" or container.data["node_type"] == "regional"):
+            return max_response
+        else:
+            return sum_tuple(max_response, my_response)
+
+
+path = "../tests_topology/tests/rootsim-serial-threads_0-FIFO-preempt_no-sim_proc_no-seed_-1-timeout_none/simulation_results.json"
+
+
+file_json = open(path, "r")
+
+json_data = json.load(file_json)
+
+dict_global = setup_dict(json_data)
+
+
+
+print(compute_response(dict_global[0]))
+
+
---
+++ test_results.py
@@ -0,0 +1,127 @@
+import sys
+import json
+import os
+
+from collections.abc import Mapping
+
+def print_warning(a,b):
+    print("WARNING: ", end="")
+    print(a, end="")
+    print(" =/= ", end="")
+    print(b)
+
+PERCENTAGE_CHECK = 5.0
+
+def compare_U(a, b):
+    print("I think that you don't want to go here")
+    U_sum = 0.0
+    for k in ["telemetry", "transition", "command", "batch"]:
+        U_sum += b[k]["utilization_factor"]
+
+    percentage = (abs(a - U_sum) / max(a, U_sum) ) * 100.0
+    if(percentage > PERCENTAGE_CHECK):
+        print_warning(a, U_sum)
+        print("Percentage: ", end="")
+        print(percentage)
+        return True
+    return False
+
+
+
+def compare_values(a, b):
+
+    warning_check = False
+
+    if(isinstance(a, str) and isinstance(b, str)):
+        if(a != b):
+            print_warning(a, b)
+            warning_check = True
+        return warning_check
+
+    if(isinstance(a, int) and isinstance(b, int)):
+        if(a != b):
+            print_warning(a, b)
+            warning_check = True
+        return warning_check
+
+    if(isinstance(a, float) and isinstance(b, float)):
+        if(a == 0.0 and b == 0.0):
+            return warning_check
+        percentage = (abs(a - b) / max(a, b) ) * 100.0
+        if(percentage > PERCENTAGE_CHECK):
+            print_warning(a, b)
+            print("Percentage: ", end="")
+            print(percentage)
+            warning_check = True
+        return warning_check
+
+    print("WARNING: values type not found")
+    return warning_check
+
+def compare_dicts(a, b):
+
+    warning_check_final = False
+
+    if(not isinstance(a, Mapping) and not isinstance(b,Mapping)):
+        warning_check = compare_values(a, b)
+        warning_check_final = warning_check
+        return warning_check_final
+
+    if((len(a) != len(b) and "U_global" not in a) or ((len(a) != len(b) + 1 and "U_global" in a))):
+        print("ERROR: len(a) != len(b)!")
+        warning_check = True
+        return warning_check_final
+
+    for k in a:
+        if k == "U_global":
+            print("I think that you don't want to go here")
+            warning_check = compare_U(a[k], b)
+        else:
+            warning_check = compare_dicts(a[k], b[k])
+        if warning_check:
+            print(k, end="")
+            print(", ", end="")
+        warning_check_final = warning_check_final or warning_check
+    
+    return warning_check_final
+
+
+simulator_file = "./tests_topology/tests/rootsim-serial-threads_0-FIFO-preempt_no-sim_proc_no-seed_-1-timeout_none/simulation_results.json"
+fsimulator = open(simulator_file, "r")
+
+compare_file = "./tests_topology/tests/simulation_results.json"
+fcompare = open(compare_file, "r")
+
+simulator = json.load(fsimulator)
+compare = json.load(fcompare)
+
+#compare if the two list match exactly
+print(simulator == compare)
+
+#check len of the two list
+number_elements_simulator = len(simulator)
+number_elements_compare = len(compare)
+
+if(number_elements_simulator != number_elements_compare):
+    print("ERROR: number_elements_simulator != number_elements_compare => true!")
+
+#sorting
+simulator = sorted(simulator, key=lambda device: device["id"])
+compare = sorted(compare, key=lambda device: device["id"])
+
+#comparing each device in the list
+for i in range(0, number_elements_simulator):
+    warning_check = compare_dicts(simulator[i], compare[i])
+    if warning_check:
+        print("id element: ", end="")
+        print(i)
+        print()
+
+
+
+
+fcompare.close()
+
+fsimulator.close()
+
+
