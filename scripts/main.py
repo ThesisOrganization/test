b966571e49966be6b8f6ec165a9c42596498a1eb
silviodeigiudici <silviodeigiudici@gmail.com>
2020-12-17 13:00:12 +0100

    feat(New-visualization-added-and-PATH): we add a visualization and the possibility to specify a PATH in order to save the image

--- scripts/main.py
+++ scripts/main.py
@@ -6,12 +6,12 @@ import json
 colors = ["red", "green", "yellow", "black", "blue"]
 
 
-draw_histograms([2, 4, 4], ["Ciao", "hello", "bye"], "Robe super", "dd", "no", 0, 5, "red")
+draw_histograms([2, 4, 4], ["Ciao", "hello", "bye"], "Robe super", "dd", "no", 0, 5, "red", PATH="file1.png")
 
 grouped_values = [[1, 3, 6], [2, 4, 4], [5, 5, 2], [9, 5, 2], [3, 2, 1]]
-draw_grouped_histograms(grouped_values, ["Ciao", "hello", "bye"], "Robe super", "dd", "no", 0, max_groupes(grouped_values) + 1, ["robe", "hihi", "random", "yotobi", "ilgatto"], colors)
+#draw_grouped_histograms(grouped_values, ["Ciao", "hello", "bye"], "Robe super", "dd", "no", 0, max_groupes(grouped_values) + 1, ["robe", "hihi", "random", "yotobi", "ilgatto"], colors)
 
-draw_grouped_histograms(grouped_values, ["Ciao", "hello", "bye"], "Robe super", "dd", "no", 0, max_groupes(grouped_values) + 1, ["robe", "hihi", "random", "yotobi", "ilgatto"])
+draw_grouped_histograms(grouped_values, ["Ciao", "hello", "bye"], "Robe super", "dd", "no", 0, max_groupes(grouped_values) + 1, ["robe", "hihi", "random", "yotobi", "ilgatto"], PATH="file2.png")
 
 
 
@@ -24,18 +24,22 @@ exams = {"AN1":30, "FDI1":30, "GEO":24, "FIS":30, "TDP":31,
 marks = list(exams.values())
 names = list(exams.keys())
 
-draw_histograms(marks, names, "Marks of exams over time", "Exams", "Marks", 0, max(marks) + 1)
+#draw_histograms(marks, names, "Marks of exams over time", "Exams", "Marks", 0, max(marks) + 1)
 
-sum = 0
+tot = 0
 numExams = 0
 mean = []
+mean2 = []
 
 for mark in marks:
-    sum += mark
+    tot += mark
     numExams += 1
-    mean.append(sum / numExams)
+    mean.append(tot / numExams)
+    mean2.append((tot - mark) / numExams)
 
-draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1)
-draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1, "#ff00ff", "#00ffff")
-draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1, "#ff00ff")
-draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1, COLOR_POINT="#ff00ff")
+draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1, PATH="file3.png")
+#draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1, "#ff00ff", "#00ffff")
+#draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1, "#ff00ff")
+#draw_lines(mean, names, "Average of marks over time", "Exams", "Mean of marks", 0, max(mean) + 1, COLOR_POINT="#ff00ff")
+
+draw_grouped_lines([mean, mean2], names, "rofefe", "examns", "mean", 0, max(mean) + 1, ["yellow", "blue"], PATH="file4.png")
--- scripts/plot_functions/plot_graph.py
+++ scripts/plot_functions/plot_graph.py
@@ -1,5 +1,9 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+SAVE = False
+PRINT = True
 
 ####################################
 #UTILS
@@ -18,7 +22,7 @@ def max_groupes(grouped_values):
 ######################################
 
 #altezza istogrammi, nomi istrogrammi, titolo png, label x, label y, valore minimo y, valore massimo y
-def draw_histograms(values, names, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, COLOR=None):
+def draw_histograms(values, names, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, COLOR=None, PATH=None):
 
     ax = plt.subplot(111)
     ax.set_ylim(bottom=YBOTTOM, top=YTOP)
@@ -29,6 +33,7 @@ def draw_histograms(values, names, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, COLOR=N
 
     if len_names != len_values:
         print("ERROR: len_names != len_values")
+        plt.close()
         return
 
     x_pos = np.arange(len_names)
@@ -50,12 +55,14 @@ def draw_histograms(values, names, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, COLOR=N
     #    yint.append(int(each))
     #plt.yticks(yint) #serve per decidere che valori mettere sulle y
 
-
-    plt.show()
+    if SAVE:
+        plt.savefig(PATH)
+    if PRINT:
+        plt.show()
     plt.close()
 
 
-def draw_grouped_histograms(grouped_values, names, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, legend_labels, colors=None):
+def draw_grouped_histograms(grouped_values, names, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, legend_labels, colors=None, PATH=None):
     
     WIDTH = 0.35
     DISTANCE_BETWEEN_GROUPS = 0.15
@@ -72,6 +79,12 @@ def draw_grouped_histograms(grouped_values, names, TITLE, XLABEL, YLABEL, YBOTTO
 
     if len(legend_labels) != num_histo_per_group:
         print("len(legend_labels) != num_histo_per_group")
+        plt.close()
+        return
+
+    if colors != None and len(colors) != num_histo_per_group:
+        print("len(colors) != num_histo_per_group")
+        plt.close()
         return
 
     group_space = num_histo_per_group * WIDTH + DISTANCE_BETWEEN_GROUPS
@@ -81,6 +94,7 @@ def draw_grouped_histograms(grouped_values, names, TITLE, XLABEL, YLABEL, YBOTTO
     for i in range(0, num_histo_per_group):
         if len(grouped_values[i]) != len_names:
             print("ERROR: grouped_values != len_names")
+            plt.close()
             return
         if colors != None:
             plt.bar(x_pos + WIDTH*i, grouped_values[i], width=WIDTH, label=legend_labels[i], color=colors[i])
@@ -102,11 +116,73 @@ def draw_grouped_histograms(grouped_values, names, TITLE, XLABEL, YLABEL, YBOTTO
     plt.xlabel(XLABEL, fontsize=12)
     plt.ylabel(YLABEL, fontsize=12, rotation=0)
 
-    plt.show()
+    if SAVE:
+        plt.savefig(PATH)
+    if PRINT:
+        plt.show()
     plt.close()
 
 
-def draw_lines(y_number_values, x_number_values, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, COLOR=None, COLOR_POINT=None):
+def draw_grouped_lines(y_grouped_number_values, x_number_values, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, colors=None, PATH=None):
+
+    ax = plt.subplot(111)
+    ax.set_ylim(bottom=YBOTTOM, top=YTOP)
+    ax.yaxis.set_label_coords(-0.03, 1.02)
+    
+
+    len_x = len(x_number_values)
+
+    x_pos = np.arange(len_x)
+
+    number_grouped = len(y_grouped_number_values)
+
+    if colors != None and len(colors) != number_grouped:
+        print("len(colors) != num_histo_per_group")
+        plt.close()
+        return
+
+    for i in range(0, number_grouped):
+        y_number_values = y_grouped_number_values[i]
+
+        len_y = len(y_number_values)
+        if len_x != len_y:
+            print("ERROR: len_x != len_y")
+            plt.close()
+            return
+        
+        if colors != None:
+            plt.plot(x_number_values, y_number_values, '.-b', linewidth=1, markersize=7, color=colors[i], marker = 'o')
+        else:
+            plt.plot(x_number_values, y_number_values, '.-b', linewidth=1, markersize=7, marker = 'o')
+
+
+
+
+    #if COLOR != None and COLOR_POINT != None:
+    #    plt.plot(x_number_values, y_number_values, '.-b', linewidth=1, markersize=7, color=COLOR, marker = 'o', markerfacecolor = COLOR_POINT)
+    #elif COLOR != None:
+    #    plt.plot(x_number_values, y_number_values, '.-b', linewidth=1, markersize=7, color=COLOR, marker = 'o')
+    #elif COLOR_POINT != None:
+    #    plt.plot(x_number_values, y_number_values, '.-b', linewidth=1, markersize=7, marker = 'o', markerfacecolor = COLOR_POINT)
+    #else:
+    #    plt.plot(x_number_values, y_number_values, '.-b', linewidth=1, markersize=7, marker = 'o')
+
+    plt.xticks(x_pos, x_number_values, rotation= '45')
+
+    plt.title(TITLE, fontsize=18, y=1.03)
+    plt.xlabel(XLABEL, fontsize=12)
+    plt.ylabel(YLABEL, fontsize=12, rotation=0)
+
+    plt.tick_params(axis='both', labelsize=12)
+
+    if SAVE:
+        plt.savefig(PATH)
+    if PRINT:
+        plt.show()
+    plt.close()
+
+
+def draw_lines(y_number_values, x_number_values, TITLE, XLABEL, YLABEL, YBOTTOM, YTOP, COLOR=None, COLOR_POINT=None, PATH=None):
 
     ax = plt.subplot(111)
     ax.set_ylim(bottom=YBOTTOM, top=YTOP)
@@ -118,6 +194,7 @@ def draw_lines(y_number_values, x_number_values, TITLE, XLABEL, YLABEL, YBOTTOM,
 
     if len_x != len_y:
         print("ERROR: len_x != len_y")
+        plt.close()
         return
 
     #x_pos = range(0, len_x)
@@ -140,7 +217,10 @@ def draw_lines(y_number_values, x_number_values, TITLE, XLABEL, YLABEL, YBOTTOM,
 
     plt.tick_params(axis='both', labelsize=12)
 
-    plt.show()
+    if SAVE:
+        plt.savefig(PATH)
+    if PRINT:
+        plt.show()
     plt.close()
 
 
